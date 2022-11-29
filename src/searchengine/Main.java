07f0955745d3232b078f16e72d0af08a0012173d
Sreekanth V <25sreekanth@gmail.com>
2022-11-28 19:54:34 -0500

    feat: spell check integration

--- src/searchengine/Main.java
+++ src/searchengine/Main.java
@@ -2,6 +2,7 @@ package searchengine;
 
 import searchengine.Crawler;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import searchengine.Convert_to_text;
@@ -10,12 +11,15 @@ public class Main {
 
 	public static void main(String args[]) throws java.lang.Exception {
 		SearchFrequency searchFrequency = new SearchFrequency();
-		BestDeal b =new BestDeal();
-		
+		BestDeal b = new BestDeal();
+		SpellCheck spellCheck = new SpellCheck();
+		Wordranks wc = new Wordranks();
+
+		ArrayList<String> wordCorrection = new ArrayList<String>();
+
 		String url = "https://www.royallepagebinder.com/residential-properties/";
 
-		System.out.println("Fectching all the Html Pages from " + url);
-
+		System.out.println("Fetching all the Html Pages from " + url);
 		Crawler.crawl(url, 0);
 
 //		for(String link: Crawler.uniqueLinks) {
@@ -23,45 +27,79 @@ public class Main {
 //		}
 
 		System.out.println("\nProcess completed for fetching Html pages!!");
-
 		Convert_to_text.convert_html_to_text();
 
-		Wordranks wc = new Wordranks();
+		
 		b.addToPriorityQueue();
 
-		// System.in is a standard input stream
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a city name: ");
-		String city = "";
-		city = sc.nextLine();
-		searchFrequency.updateSearchHistory(city);
-
-		wc.getPageRank(city);
-
-		System.out.print("\n*******************************\n\n");
-		System.out.print("Enter number of bedrooms: ");
-		String bedroomNo = sc.nextLine();
-		System.out.println(b.bestDeal(Integer.valueOf(bedroomNo)));
-
-		System.out.println("Do you want to search again ? Press Y or N");
-		String input = sc.nextLine();
-
-		if (input.equalsIgnoreCase("y")) {
-			while (input.equalsIgnoreCase("y")) {
-				System.out.print("Enter a city name: ");
-				city = sc.nextLine();
-				searchFrequency.updateSearchHistory(city);
-				wc.getPageRank(city);
-				System.out.print("\n*******************************\n\n");
-				System.out.print("Enter number of bedrooms: ");
-				bedroomNo = sc.nextLine();
-				System.out.println(b.bestDeal(Integer.valueOf(bedroomNo)));
-				System.out.println("Do you want to search again ? Press Y or N");
-				input = sc.nextLine();
-			}
-		}
-		searchFrequency.display();
+//		System.out.println("Search History\n");
+//		searchFrequency.display();
+//		System.out.println("\n");
+//
+//		System.out.print("Enter a city name: ");
+//		String city = "";
+//		city = sc.nextLine();
+//
+//		wordCorrection = spellCheck.correction(city);
+//
+//		while(wordCorrection.size() > 0) {
+//			System.out.println("Are you sure the spelling is correct. Please select a word from the given list");
+//			System.out.println(wordCorrection);
+//			
+//			System.out.print("Enter a city name: ");
+//		    city = "";
+//			city = sc.nextLine();
+//
+//			wordCorrection = spellCheck.correction(city);
+//		}
+//
+//		searchFrequency.updateSearchHistory(city);
+//		wc.getPageRank(city);
+//
+//		System.out.print("\n*******************************\n\n");
+//		System.out.print("Enter number of bedrooms: ");
+//		String bedroomNo = sc.nextLine();
+//		System.out.println(b.bestDeal(Integer.valueOf(bedroomNo)));
+//
+//		System.out.println("Do you want to search again ? Press Y or N");
+//		String input = sc.nextLine();
 		
+		Scanner sc = new Scanner(System.in);
+		
+		String input = "y";
+		String city;
+		String bedroomNo;
+
+//		if (input.equalsIgnoreCase("y")) {
+		while (input.equalsIgnoreCase("y")) {
+			System.out.print("Enter a city name: ");
+			city = sc.nextLine();
+
+			wordCorrection = spellCheck.correction(city);
+
+			while (wordCorrection.size() > 0) {
+				System.out.println("Are you sure the spelling is correct. Please select a word from the given list");
+				System.out.println(wordCorrection);
+
+				System.out.print("Enter a city name: ");
+				city = "";
+				city = sc.nextLine();
+
+				wordCorrection = spellCheck.correction(city);
+			}
+
+
+			searchFrequency.updateSearchHistory(city);
+			wc.getPageRank(city);
+			System.out.print("\n*******************************\n\n");
+			System.out.print("Enter number of bedrooms: ");
+			bedroomNo = sc.nextLine();
+			System.out.println(b.bestDeal(Integer.valueOf(bedroomNo)));
+			System.out.println("Do you want to search again ? Press Y or N");
+			input = sc.nextLine();
+		}
+//		}
+
 		System.out.println("\nThanks. The Demo ends here. Have a good day.");
 
 	}
